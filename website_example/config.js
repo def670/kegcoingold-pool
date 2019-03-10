0aaa03ad68506da607fb76a79897d059cbcfec35
Christopher Matuszak <admin@twisted-networks.com>
2019-03-09 23:51:17 -0500

    Initial Upload
    Initial Upload of working pool

---
+++ website_example/config.js
@@ -0,0 +1,13 @@
+var api = "URL TO YOUR POOL API";
+
+var email = "your@email.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://keg-explorer.twisted-networks.com/?hash={id}#blockchain_block";
+var transactionExplorer = "http://keg-explorer.twisted-networks.com/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
